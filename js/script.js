cbcf475c66571466faaaa7e4d08f331037aaaadb
Jeremy <jeremyperret1@gmail.com>
2024-12-04 22:39:08 +0100

    modification pour adapter au tel

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
+let previousElement = null;
+
+function applyFlexToHashElement() {
+  // Récupère la partie de l'URL après le #
+  const hash = window.location.hash.substring(1);
+
+  // Vérifie si un hash est présent
+  if (hash) {
+    // Sélectionne l'élément correspondant
+    const element = document.getElementById(hash);
+
+    // Vérifie si l'élément existe
+    if (element) {
+      // Réinitialise le style de l'élément précédent
+      if (previousElement) {
+        previousElement.style.display = "none";
+      }
+
+      // Applique le style display: flex à l'élément actuel
+      element.style.display = "flex";
+
+      // Met à jour l'élément précédent
+      previousElement = element;
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Applique le style lors du chargement initial de la page
+  applyFlexToHashElement();
+
+  // Écoute les changements de hash
+  window.addEventListener("hashchange", applyFlexToHashElement);
+});
+
 // Récupérer tous les boutons pour afficher les popups
 const showPopupButtons = document.querySelectorAll(".showPopupButton");
 const closePopupButtons = document.querySelectorAll(".closePopupButton");
